4772eb72af3a169a324f22badf6699bb2e619c1b
har015 <ruiz9984@icloud.com>
2024-04-22 20:36:35 -0400

    modified code

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+// Redirect requests to the root URL to Home.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Login.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
